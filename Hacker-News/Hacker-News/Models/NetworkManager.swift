b49f5ca830e8c900b415d00c9e8371bbf40bbe9a
Jules <ju.pabst@gmail.com>
2024-02-01 13:45:15 +0100

    added fetchData() function and added first part of JSON Parsing

--- Hacker-News/Hacker-News/Models/NetworkManager.swift
+++ Hacker-News/Hacker-News/Models/NetworkManager.swift
@@ -7,6 +7,20 @@
 
 import Foundation
 
-struct NetworkManager {
-    
+class NetworkManager {
+    func fetchData(){
+        if let url = URL(string: "https://hn.algolia.com/api/v1/search?tags=front_page") {
+                    let session = URLSession(configuration: .default)
+                    let task = session.dataTask(with: url) { data, response, error in
+                        if error != nil {
+                            print(error!)
+                            return
+                        } else {
+                            let decoder = JSONDecoder()
+                        }
+                    }
+                    task.resume()
+                }
+
+    }
 }
