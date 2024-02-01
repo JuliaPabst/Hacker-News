8c5d9bb42852af0196fbeb5bbc022660b247f4c1
Jules <ju.pabst@gmail.com>
2024-02-01 14:23:06 +0100

    created Views folder and put ContentView in it. Created new SwiftUI file DetailView

---
+++ Hacker-News/Hacker-News/Views/DetailView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailView.swift
+//  Hacker-News
+//
+//  Created by Julia Pabst on 01.02.24.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailView()
+}
